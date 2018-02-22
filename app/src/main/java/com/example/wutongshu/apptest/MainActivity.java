bc6d2d3389004c1b62ce4e68281cf58a26e20271
wutongshuxiadoumao <755937400@qq.com>
2018-02-22 18:31:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wutongshu/apptest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wutongshu.apptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
